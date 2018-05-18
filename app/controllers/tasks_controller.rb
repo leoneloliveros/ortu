ca4a530cecf1cbe5b4b03a4241d429ed6722fc95
Leonel Oliveros <leoneloliveros.co@gmail.com>
2018-05-18 17:18:53 -0500

    Creation of task (CRUD)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,34 @@
 class TasksController < ApplicationController
 	def new
 		@task = Task.new
+		@title1 = "Crear Tarea"
+		@title2 = @title1
 	end
 	def create
 		@task = Task.new(task_params)
+		@task.completed = false
 		@task.save
 	end
+	def complete
+		@task = Task.find(params[:id])
+		@task.toggle(:completed)
+
+		@task.save
+	end
+	def edit
+		@task = Task.find(params[:id])
+		@title1 = "Editar Tarea"
+		@title2 = "Aceptar"
+	end
+	def update
+		@task = Task.find(params[:id])
+		@task.update(task_params)
+	end
+	def destroy
+		@task = Task.find(params[:id])
+		@task.destroy		
+	end
+	
 	private
 	def task_params
 		params.require(:task).permit(:title, :description).merge(user: current_user)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 	root to: "welcome#index"
   	get 'welcome/index'
   	devise_for :users
-  	resources :tasks
+  	resources :tasks do
+  		patch :complete, on: :member
+  	end
   	resources :profiles, only: [:show]
   	resources :posts do
   		resources :pictures
